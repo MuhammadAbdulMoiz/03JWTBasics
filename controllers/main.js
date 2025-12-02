e30c7859c0767104dd994147adaf747af4a50ef3
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-12-02 11:56:39 +0500

    jwt signing and verifing

--- controllers/main.js
+++ controllers/main.js
@@ -7,14 +7,37 @@ const login = async (req, res) => {
         throw new CustomAPIError('Please provide User Name and Password',400);
     }
     const id = new Date().getDate();
-    const token = jwt.sign({id, username},process.env.secret,{expiresIn:'1d'});
-    console.log(username, password); 
+    const claims = {
+        sub: id, 
+        uname: username,
+        role: 'user'
+    }
+    const key = process.env.secret;
+    const options = {
+        expiresIn: '15m',
+        issuer: 'api-v1'
+    }
+    const token = jwt.sign(claims, key, options);
     res.status(200).json({msg:'user created', token});
 }
 
 const dashboard = async (req, res) => {
-    const luckyNo = Math.floor(Math.random()*100);
-    res.status(200).json({msg:`Hello, oni`, secret:`Here is your Secret Key ${luckyNo}`});
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer')){
+        throw new CustomAPIError('No Token Present', 401);
+    }
+    const tkn = authHeader.split(' ')[1]
+    const key = process.env.secret;
+    try{
+        const decode = jwt.verify(tkn, key)
+        console.log(decode)
+        const luckyNo = Math.floor(Math.random()*100);
+        res.status(200).json({msg:`Hello, ${decode.uname}`, secret:`Here is your Secret Key ${luckyNo}`});
+    }
+    catch{
+        throw new CustomAPIError('No authorized to access this route', 401);
+    }
+    
 }
 
 module.exports = {
